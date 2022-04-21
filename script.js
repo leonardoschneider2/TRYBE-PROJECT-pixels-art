821a86a77e3e6411df823b9724290d1e39ff0626
leonardo-schneider2 <leocoelho256@gmail.com>
2022-04-21 14:51:20 -0300

    lint

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 const quadroDePintura = document.getElementById('pixel-board');
 
 function firstFrame(num) {
-  if (num > 50) {
-    num = 50;
+  let number = num;
+  if (number > 50) {
+    number = 50;
   }
-  if (num < 5) {
-    num = 5;
+  if (number < 5) {
+    number = 5;
   }
-  for (let i = 0; i < num; i += 1) {
+
+  for (let i = 0; i < number; i += 1) {
     const linhaDePintura = document.createElement('div');
     linhaDePintura.className = 'linha';
     quadroDePintura.appendChild(linhaDePintura);
 
-    for (let j = 0; j < num; j += 1) {
+    for (let j = 0; j < number; j += 1) {
       const pixelDePintura = document.createElement('div');
       pixelDePintura.className = 'pixel';
       linhaDePintura.appendChild(pixelDePintura);
@@ -20,9 +22,21 @@ function firstFrame(num) {
   }
 }
 
+function changeColor() {
+  function aleColor() {
+    return (Math.random() * 255);
+  }
+  const back1 = `rgb(${aleColor()}, ${aleColor()}, ${aleColor()})`;
+  const back2 = `rgb(${aleColor()}, ${aleColor()}, ${aleColor()})`;
+  const back3 = `rgb(${aleColor()}, ${aleColor()}, ${aleColor()})`;
+
+  document.querySelector('#color1').style.backgroundColor = back1;
+  document.querySelector('#color2').style.backgroundColor = back2;
+  document.querySelector('#color3').style.backgroundColor = back3;
+}
+
 firstFrame(5);
 changeColor();
-
 
 function colorSelect(event) {
   const whoWasClicked = event.target;
@@ -60,19 +74,6 @@ function paintingAllWhite() {
 
 document.querySelector('#clear-board').addEventListener('click', paintingAllWhite);
 
-function changeColor() {
-  function aleColor() {
-    return (Math.random() * 255);
-  }
-  const back1 = `rgb(${aleColor()}, ${aleColor()}, ${aleColor()})`;
-  const back2 = `rgb(${aleColor()}, ${aleColor()}, ${aleColor()})`;
-  const back3 = `rgb(${aleColor()}, ${aleColor()}, ${aleColor()})`;
-
-  document.querySelector('#color1').style.backgroundColor = back1;
-  document.querySelector('#color2').style.backgroundColor = back2;
-  document.querySelector('#color3').style.backgroundColor = back3;
-}
-
 document.querySelector('#change-color').addEventListener('click', changeColor);
 
 function resetColor() {
@@ -83,17 +84,14 @@ function resetColor() {
 
 document.querySelector('#reset-color').addEventListener('click', resetColor);
 
-document.querySelector('#generate-board').addEventListener('click', createFrame);
-
 function createFrame() {
   const number = document.querySelector('#board-size').value;
-  if(number !== '') {
-    // const newFrame = document.createElement('div');
-    /*newFrame.className = 'pixel-board';*/
-    //document.getElementById('corpo-sessao').appendChild(newFrame);
+  if (number !== '') {
     document.querySelector('#pixel-board').textContent = '';
     firstFrame(number);
   } else {
     return alert('Board inválido!');
   }
 }
+
+document.querySelector('#generate-board').addEventListener('click', createFrame);
