b3f19d92081198c5d28194813c451a3eb86ffeb7
leonardo-schneider2 <leocoelho256@gmail.com>
2022-04-21 11:51:21 -0300

    EXERCICIOS NECESSÁRIOS E LINT COMPLETO!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let quadroDePintura = document.getElementById('pixel-board');
+const quadroDePintura = document.getElementById('pixel-board');
 
 for (let i = 0; i < 5; i += 1) {
   const linhaDePintura = document.createElement('div');
@@ -17,23 +17,21 @@ function colorSelect(event) {
   const fatherWhoWasClicked = event.currentTarget;
   if (whoWasClicked !== fatherWhoWasClicked) {
     const colors = document.querySelectorAll('.color');
-    
     for (let i = 0; i < colors.length; i += 1) {
       colors[i].classList.remove('selected');
     }
     whoWasClicked.className += ' selected';
-  } 
+  }
 }
 
 document.querySelector('#color-palette').addEventListener('click', colorSelect);
 
-function colorPaint (event) {
+function colorPaint(event) {
   const whoWasClicked = event.target;
   const fatherWhoWasClicked = event.currentTarget;
   if (whoWasClicked !== fatherWhoWasClicked) {
     const colorSelected = document.querySelector('.selected');
-    const background = window.getComputedStyle(colorSelected).getPropertyValue('background-color'); 
-    
+    const background = window.getComputedStyle(colorSelected).getPropertyValue('background-color');
     whoWasClicked.style.backgroundColor = background;
   }
 }
@@ -49,3 +47,29 @@ function paintingAllWhite() {
 }
 
 document.querySelector('#clear-board').addEventListener('click', paintingAllWhite);
+
+function changeColor() {
+  function aleColor() {
+    return (Math.random() * 255);
+  }
+  const titleColor = `rgb(${aleColor()}, ${aleColor()}, ${aleColor()})`;
+  const back1 = `rgb(${aleColor()}, ${aleColor()}, ${aleColor()})`;
+  const back2 = `rgb(${aleColor()}, ${aleColor()}, ${aleColor()})`;
+  const back3 = `rgb(${aleColor()}, ${aleColor()}, ${aleColor()})`;
+
+  document.querySelector('#title').style.color = titleColor;
+  document.querySelector('#color1').style.backgroundColor = back1;
+  document.querySelector('#color2').style.backgroundColor = back2;
+  document.querySelector('#color3').style.backgroundColor = back3;
+}
+
+document.querySelector('#change-color').addEventListener('click', changeColor);
+
+function resetColor() {
+  document.querySelector('#title').style.color = 'black';
+  document.querySelector('#color1').style.backgroundColor = 'rgb(255, 89, 89)';
+  document.querySelector('#color2').style.backgroundColor = 'rgb(50, 121, 50)';
+  document.querySelector('#color3').style.backgroundColor = 'rgb(96, 96, 255)';
+}
+
+document.querySelector('#reset-color').addEventListener('click', resetColor);
